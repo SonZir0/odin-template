e468b9a4f6ea14ae7d913670c905a918c0da6a33
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2024-07-24 15:09:14 +0400

    Add jest and configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,33 @@
-import globals from "globals";
+import globals from 'globals';
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
-	eslintConfigPrettier,
-	{
-		ignores: ["eslint.config.?js"]
-	},
-	...compat.extends("airbnb-base",
-   						"prettier"),
+    eslintConfigPrettier,
+    {
+        ignores: ['eslint.config.?js'],
+    },
+    {
+        overrides: [
+            {
+                files: ['tests/**/*'],
+                env: {
+                    jest: true,
+                },
+            },
+        ],
+    },
+    ...compat.extends('airbnb-base', 'prettier'),
 ];
